ce4c969805c70992c9fce383f3585d036bf9cd5d
Michael Bestas <mkbestas@lineageos.org>
2024-06-15 03:24:16 +0300

    Merge tag 'android-14.0.0_r50' into staging/lineage-21.0_merge-android-14.0.0_r50
    Android 14.0.0 Release 50 (AP2A.240605.024)
    
    # -----BEGIN PGP SIGNATURE-----
    #
    # iF0EABECAB0WIQRDQNE1cO+UXoOBCWTorT+BmrEOeAUCZmdzmgAKCRDorT+BmrEO
    # eP7dAJ42VrzKYprSm+hHEe9RGjMQ/5mkEwCfVJoQ5sdiW8Y4D8qOcwYATY6xry8=
    # =gy7t
    # -----END PGP SIGNATURE-----
    # gpg: Signature made Tue Jun 11 00:43:54 2024 EEST
    # gpg:                using DSA key 4340D13570EF945E83810964E8AD3F819AB10E78
    # gpg: Good signature from "The Android Open Source Project <initial-contribution@android.com>" [marginal]
    # gpg: initial-contribution@android.com: Verified 2467 signatures in the past
    #      2 years.  Encrypted 4 messages in the past 2 years.
    # gpg: WARNING: This key is not certified with sufficiently trusted signatures!
    # gpg:          It is not certain that the signature belongs to the owner.
    # Primary key fingerprint: 4340 D135 70EF 945E 8381  0964 E8AD 3F81 9AB1 0E78
    
    # By Elliott Hughes (51) and others
    # Via Automerger Merge Worker (333) and others
    * tag 'android-14.0.0_r50': (102 commits)
      ReadPadSegmentNote: Skip PT_NOTEs that are beyond the end of the file
      Revert "RELAND: bionic: loader: Extend LOAD segment VMAs"
      Revert "RELAND: bionic: loader: Extend GNU_RELRO protection"
      Correct the nullability annotation in posix_spawnattr_init()
      Add _Fork().
      Remove unnecessary includes
      Add ..DEPRECATED_PAGE_SIZE macro
      CtsBionicAppTestCases has moved to presubmit.
      RELAND: bionic: loader: Extend GNU_RELRO protection
      RELAND: bionic: loader: Extend LOAD segment VMAs
      __bionic_get_shell_path(): explanatory comment.
      Add CtsBionicAppTestCases to TEST_MAPPING.
      cxa_demangle_test: expand accepted outputs
      Improve android_set_abort_message() documentation.
      Mention ApplicationExitInfo in docs
      Add CtsBionicAppTestCases to postsubmit
      Stop trying to be clever with sysconf(_SC_NGROUPS_MAX).
      Fix copyright headers on new tests.
      Clarify _PC_REC_INCR_XFER_SIZE and _PC_REC_MAX_XFER_SIZE.
      Add another link to the benchmark docs.
      ...
    
    Change-Id: I6e76f4b8bbaae7b692c41324b854102742a26af5

--- linker/linker_phdr.cpp
+++ linker/linker_phdr.cpp
@@ -42,6 +42,7 @@
 #include "linker_debug.h"
 #include "linker_utils.h"
 
+#include "private/bionic_asm_note.h"
 #include "private/CFIShadow.h" // For kLibraryAlignment
 #include "private/elf_note.h"
 
@@ -141,8 +142,18 @@ static int GetTargetElfMachine() {
                                       MAYBE_MAP_FLAG((x), PF_R, PROT_READ) | \
                                       MAYBE_MAP_FLAG((x), PF_W, PROT_WRITE))
 
-// Default PMD size for x86_64 and aarch64 (2MB).
-static constexpr size_t kPmdSize = (1UL << 21);
+static const size_t kPageSize = page_size();
+
+/*
+ * Generic PMD size calculation:
+ *    - Each page table (PT) is of size 1 page.
+ *    - Each page table entry (PTE) is of size 64 bits.
+ *    - Each PTE locates one physical page frame (PFN) of size 1 page.
+ *    - A PMD entry locates 1 page table (PT)
+ *
+ *   PMD size = Num entries in a PT * page_size
+ */
+static const size_t kPmdSize = (kPageSize / sizeof(uint64_t)) * kPageSize;
 
 ElfReader::ElfReader()
     : did_read_(false), did_load_(false), fd_(-1), file_offset_(0), file_size_(0), phdr_num_(0),
@@ -164,7 +175,8 @@ bool ElfReader::Read(const char* name, int fd, off64_t file_offset, off64_t file
       VerifyElfHeader() &&
       ReadProgramHeaders() &&
       ReadSectionHeaders() &&
-      ReadDynamicSection()) {
+      ReadDynamicSection() &&
+      ReadPadSegmentNote()) {
     did_read_ = true;
   }
 
